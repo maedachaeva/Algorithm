77e63675288676817f636063b44f736ad61e9383
Chaeyeon Park <parkchaeyeon.b@gmail.com>
2025-03-19 15:08:33 +0900

    [Bronze V] Title: 오늘 날짜, Time: 156 ms, Memory: 16988 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,9 @@
+import java.time.*;
+
+public class Main {
+    public static void main(String[] args) {
+        LocalDate currentDate = LocalDate.now();
+        
+        System.out.println(currentDate);
+    }
+}
