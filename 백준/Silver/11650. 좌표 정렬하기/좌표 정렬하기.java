f53bf846a30da5aaadd8d9e116268d0ace1bf62a
Chaeyeon Park <parkchaeyeon.b@gmail.com>
2025-03-19 15:04:18 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 700 ms, Memory: 59004 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[][] points = new int[N][2];
+
+        for (int i = 0; i < N; i++) {
+            String[] input = br.readLine().split(" ");
+            points[i][0] = Integer.parseInt(input[0]);
+            points[i][1] = Integer.parseInt(input[1]);
+        }
+
+        Arrays.sort(points, (a, b) -> (a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]));
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            sb.append(points[i][0]).append(" ").append(points[i][1]).append("\n");
+        }
+
+        System.out.print(sb);
+    }
+}
