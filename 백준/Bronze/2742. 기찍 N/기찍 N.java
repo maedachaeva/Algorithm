9be83468709f14cd21422c66fae15ba57957e7a6
Chaeyeon Park <parkchaeyeon.b@gmail.com>
2025-03-19 16:06:42 +0900

    [Bronze IV] Title: 기찍 N, Time: 780 ms, Memory: 34008 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int N = scanner.nextInt();
+
+        for (int i = N; i >= 1; i--) {
+            System.out.println(i);
+        }
+    }
+}
