ba52b8b68f58e8332803fbdbc31b854a150691f6
Chaeyeon Park <parkchaeyeon.b@gmail.com>
2025-03-19 16:04:41 +0900

    [Silver III] Title: 소수 구하기, Time: 632 ms, Memory: 32796 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -0,0 +1,29 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] input = br.readLine().split(" ");
+        int M = Integer.parseInt(input[0]);
+        int N = Integer.parseInt(input[1]);
+
+        boolean[] isNotPrime = new boolean[N + 1];
+        isNotPrime[0] = true;
+        isNotPrime[1] = true;
+
+        for (int i = 2; i * i <= N; i++) {
+            if (!isNotPrime[i]) {
+                for (int j = i * i; j <= N; j += i) {
+                    isNotPrime[j] = true;
+                }
+            }
+        }
+
+        for (int i = M; i <= N; i++) {
+            if (!isNotPrime[i]) {
+                System.out.println(i);
+            }
+        }
+    }
+}
