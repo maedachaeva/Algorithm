294a6da07db51479d2e8b11ad8d313d98e3e7a50
Chaeyeon Park <parkchaeyeon.b@gmail.com>
2025-03-19 15:04:51 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 104 ms, Memory: 14184 KB -BaekjoonHub

---
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        String input = br.readLine();
+
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if (Character.isLowerCase(c)) {
+                sb.append(Character.toUpperCase(c));
+            } else {
+                sb.append(Character.toLowerCase(c));
+            }
+        }
+        System.out.println(sb.toString());
+    }
+}
